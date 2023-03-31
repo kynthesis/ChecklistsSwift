c1a1a46db7ac23e52c698d968f0d6a6df37e89dd
Khoa D. Vo <khoa.synthesis@gmail.com>
2023-03-31 18:30:39 +0700

    add checklists foundation

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Khoa D. Vo on 3/31/23.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
